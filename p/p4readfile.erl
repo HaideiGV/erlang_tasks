8447de8ddb265bbe7b5d9933fdf4af1cf4692932
Viacheslav Haidei <viacheslav.haidei@nextiva.com>
2016-11-12 02:46:56 +0200

    SOLVED.Function for print file

--- p/p4readfile.erl
+++ p/p4readfile.erl
@@ -1,5 +1,7 @@
 -module(p4readfile).
--export([file_out/1]).
+-export([out/1]).
 
-file_out(file_name)->{ok, Bin}=file:read_file(file_name).
+out(Name)->out({ok, <<>>}, Name).
+
+out({ok, _}, Name)->file:read_file(Name).
 
